c279fd03a5cbc2f5207ba183b4676fcebe9242da
Kamil Yasin ABLAY <KamilyasinAblay@hotmail.com>
2022-12-11 14:58:13 +0300

    oky

---
+++ AdapterDesignPattern_ChatGPT/Program.cs
@@ -0,0 +1,39 @@
+﻿public interface ITarget
+{
+  void Request();
+}
+
+public class Adapter : ITarget
+{
+  private Adaptee _adaptee;
+
+  public Adapter(Adaptee adaptee)
+  {
+    _adaptee = adaptee;
+  }
+
+  public void Request()
+  {
+    // Delegate the call to the adaptee's method
+    _adaptee.SpecificRequest();
+  }
+}
+
+public class Adaptee
+{
+  public void SpecificRequest()
+  {
+    // Do something specific
+  }
+}
+
+public class Client
+{
+  static void Main(string[] args)
+  {
+    Adaptee adaptee = new Adaptee();
+    ITarget target = new Adapter(adaptee);
+
+    target.Request();
+  }
+}
